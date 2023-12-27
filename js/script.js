ecc61cc4711b0a3c81c508467775fe76bfe9e77a
kuzpugonka <116354773+kuzpugonka@users.noreply.github.com>
2023-12-27 11:48:07 +0300

    dz 4day render goods

--- js/script.js
+++ js/script.js
@@ -130,5 +130,102 @@ const restRender = () => {
   }
 };
 
+const goodsRender = () => {
+  const container = document.querySelector("#goods-container");
+
+  const restArray = [
+    {
+      id: 0,
+      title: "Ролл угорь стандарт",
+      text: "Рис, угорь, соус унаги, кунжут, водоросли нори.",
+      price: 250,
+      image: "image.jpg",
+    },
+    {
+      id: 1,
+      title: "Калифорния лосось стандарт",
+      text: "Рис, лосось, авокадо, огурец, майонез, икра масаго, водоросли нори.",
+      price: 395,
+      image: "image-2.jpg",
+    },
+    {
+      id: 2,
+      title: "Окинава стандарт",
+      text: "Рис, креветка отварная, сыр сливочный, лосось, огурец свежий...",
+      price: 250,
+      image: "image-4.jpg",
+    },
+    {
+      id: 3,
+      title: "Цезарь маки хl",
+      text: "Рис, куриная грудка копченая, икра масаго, томат, айсберг, соус цезарь...",
+      price: 250,
+      image: "image-1.jpg",
+    },
+    {
+      id: 4,
+      title: "Ясай маки стандарт 185 г",
+      text: "Рис, помидор свежий, перец болгарский, авокадо, огурец, айсберг",
+      price: 250,
+      image: "image-3.jpg",
+    },
+    {
+      id: 5,
+      title: "Ролл с креветкой стандарт",
+      text: "Рис, водоросли нори, креветки отварные, сыр сливочный, огурцы",
+      price: 250,
+      image: "image-5.jpg",
+    },
+  ];
+
+  const loading = () => {
+    container.innerHTML =
+      '<h1 style="width: 100%; text-align: center">Загрузка</h1>';
+  };
+
+  const renderGoods = (array) => {
+    container.innerHTML = "";
+
+    array.forEach((cart) => {
+      container.insertAdjacentHTML(
+        "beforeend",
+        ` <div class="products-cart">
+            <div class="products-cart__img">
+              <img src="./img/goods/${cart.image}" alt="${cart.title}" />
+            </div>
+            <div class="products-cart__desc">
+              <div class="products-cart__desc-row">
+                <h5 class="products-cart__desc_name">${cart.title}</h5>
+              </div>
+
+              <div class="products-cart__desc-row">
+                <p class="products-cart__desc_text">${cart.text}</p>
+              </div>
+
+              <div class="products-cart__desc-row">
+                <div class="products-cart__desc-controls">
+                  <button class="btn btn-primery">
+                    <img src="./img/icons/shopping-cart.svg" alt="user" />В корзину
+                  </button>
+                  <span class="products-cart__desc-controls_price">${cart.price}&nbsp;₽</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+      );
+    });
+  };
+
+  if (container) {
+    loading();
+
+    setTimeout(() => {
+      renderGoods(restArray);
+    }, 1000);
+  }
+};
+
 modalControls();
 restRender();
+goodsRender();
